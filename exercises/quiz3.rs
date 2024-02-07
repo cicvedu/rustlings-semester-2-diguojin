208b26a89fa7666db7cba6002803bae42b0803c4
diguojin <diguojin@163.com>
2024-02-07 16:21:02 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,16 +18,36 @@
 
 // I AM NOT DONE
 
+pub trait ScoredCard {
+    fn print(&self) -> String;
+}
+
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+pub struct ReportCardAlphabetical {
+    pub grade: String,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ScoredCard for ReportCard {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            self.student_name, self.student_age, self.grade
+        )
+    }
+}
+impl ScoredCard for ReportCardAlphabetical {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            self.student_name, self.student_age, self.grade
+        )
     }
 }
 
@@ -51,8 +71,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = ReportCardAlphabetical {
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
